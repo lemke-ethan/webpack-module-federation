e5d7e34aee179731a2776993fdf8d31a150a08f7
Ethan Lemke <lemke.ethan@gmail.com>
2023-08-23 02:28:37 +0000

    server: app.controller: add auth guard and login endpoint, remove unused app service

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -1,16 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+
 @Module({
+  controllers: [AppController],
+  providers: [UsersService],
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
