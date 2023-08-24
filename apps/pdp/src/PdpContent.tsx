66d8db66fd0f196ce954ecbbc38f211e6f47c991
Ethan Lemke <lemke.ethan@gmail.com>
2023-08-24 04:21:16 +0000

    apps/pdp: PdpContent.tsx: add rest of product info, add styles

--- apps/pdp/src/PdpContent.tsx
+++ apps/pdp/src/PdpContent.tsx
@@ -1,3 +1,4 @@
+import "./styles/PdpContent.css"
 import React, { useEffect, useState } from "react"
 import { getProductById, currency } from "home/products";
 import { IProduct } from "server";
@@ -19,5 +20,25 @@ export function PdpContent() {
     }, [product])
 
     if (product === null) { return }
-    return <div className="pdp-content">Product {productId}</div>
+    return (
+        <div className="pdp-content">
+            <div>
+                <img className="product-image" src={product.image} alt={product.name} />
+            </div>
+            <div className="product-info">
+                <h1 className="product-name">
+                    {product.name}
+                </h1>
+                <div className="product-price">
+                    {currency.format(product.price)}
+                </div>
+                <div className="product-description">
+                    {product.description}
+                </div>
+                <div className="product-long-description">
+                    {product.longDescription}
+                </div>
+            </div>
+        </div>
+    )
 }
