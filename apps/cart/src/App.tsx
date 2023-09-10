1b8fcc3348887cd5723b2665f647bad2d9c0d01d
Ethan Lemke <lemke.ethan@gmail.com>
2023-09-10 01:20:12 +0000

    init the cart app

---
+++ apps/cart/src/App.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Header } from "home/Header";
+import { Footer } from "home/Footer";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    }
+]);
+
+function App() {
+    return (
+        <div className="app">
+            <Header subHeader="Cart" />
+            <div className="content">
+                Cart
+            </div>
+            <Footer copyright="2023" />
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root");
+if (rootElement === null) {
+    throw new Error("Failed to find the application's root element.")
+}
+const root = createRoot(rootElement);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
---
+++ apps/cart/webpack.config.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { packageNamespace } = require("@types/home")
+
+module.exports = {
+  output: {
+    publicPath: `http://localhost:${process.env.CART_PORT}/`,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".json"],
+  },
+  devServer: {
+    port: process.env.CART_PORT,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css)$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      // needs to meet js variable name restrictions
+      name: "pdp",
+      filename: "remoteEntry.js",
+      remotes: {
+        // the key is used in the import statement for this package
+        // the first part of the value needs to match the name of the mf plugin of home
+        // the second part of the value needs to be the URL to the remoteEntry.js file
+        home: `${packageNamespace}@${process.env.CART_HOME_REMOTE_ENTRY_URL}`
+      },
+      exposes: {
+      },
+      shared: {
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
